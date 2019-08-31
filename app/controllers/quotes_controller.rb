2d0bf0a2d7ec3447bdd614082fb03275746554e7
Olivia Goehrig <goehrigo@gmail.com>
2019-08-31 22:19:31 +0000

    quotes working fml

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,5 @@
 class QuotesController < ApplicationController
 	def index
+    @quotes = Quotes.order("RANDOM()").first
 	end
 end
--- app/models/quotes.rb
+++ app/models/quotes.rb
@@ -1,2 +1,2 @@
-class Quote < ApplicationRecord
+class Quotes < ApplicationRecord
 end
